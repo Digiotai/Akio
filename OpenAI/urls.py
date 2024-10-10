00ed870a4641da86a5a3e8b3ccdb371ecf82bb29
Digiotai <info@digiotai.com>
2024-10-10 22:31:54 +0530

    Added Failure in predictive maintenance

--- OpenAI/urls.py
+++ OpenAI/urls.py
@@ -5,13 +5,13 @@ urlpatterns = [
     #path("api/", testing, name="testing"),
    # path(r'api/register', register, name='register'),
    # path(r'api/login', loginpage, name='login'),
-   path('api/connect', connection, name='connection'),
+    path('api/connect', connection, name='connection'),
     path("api/upload", upload_and_analyze_data, name='upload_data'),
     path('api/tableinfo', get_tableinfo, name='get_table_info'),
     path("api/read_db_table_data", read_db_table_data, name='read_table_data'),
 
     # Analytical
-   path("api/gen_txt_response", gen_txt_response, name="regenerate_chart"),
+    path("api/gen_txt_response", gen_txt_response, name="regenerate_chart"),
     path("api/gen_graph_response", gen_graph_response, name="GenAIPrompt"),
     path("api/regenerate_txt_questions", regenerate_txt, name="regenerate"),
     path("api/regenerate_graph_questions", regenerate_chart, name="regenerate_chart"),
